053a9f4f31aa2429a855ca5c85ad510b3b67b572
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-10 20:58:56 -0800

    disable menu items

--- src/components/menu-items.js
+++ src/components/menu-items.js
@@ -1,46 +1,46 @@
-import React from "react";
-import { Link } from "gatsby";
-import { Consumer } from "wordsby/context/createWordsbyContext";
+// import React from "react";
+// import { Link } from "gatsby";
+// import { Consumer } from "wordsby/context/createWordsbyContext";
 
-export default ({ slug: propSlug, children, ...rest }) => {
-  if (!propSlug) {
-    return true ? (
-      <Consumer>
-        {({ wpMenu }) => {
-          // console.log(wpMenu);
-          return null;
-          // const menu = wpmenu.edges.filter(
-          //   ({ node }) => node.slug === propSlug
-          // );
+// export default ({ slug: propSlug, children, ...rest }) => {
+//   if (!propSlug) {
+//     return true ? (
+//       <Consumer>
+//         {({ wpMenu }) => {
+//           // console.log(wpMenu);
+//           return null;
+//           // const menu = wpmenu.edges.filter(
+//           //   ({ node }) => node.slug === propSlug
+//           // );
 
-          // const items = menu.length > 0 ? menu[0].node.items : false;
-          // // remove the WP site url from menu links.
-          // for (const item of items) {
-          //   item.url = item.url.replace(url, "");
-          // }
+//           // const items = menu.length > 0 ? menu[0].node.items : false;
+//           // // remove the WP site url from menu links.
+//           // for (const item of items) {
+//           //   item.url = item.url.replace(url, "");
+//           // }
 
-          // return children ? (
-          //   children(items)
-          // ) : (
-          //   <>
-          //     {items.map(item => (
-          //       <Link key={`menu-item-${item.wordpress_id}`} to={item.url}>
-          //         {item.title}
-          //       </Link>
-          //     ))}
-          //   </>
-          // );
-        }}
-      </Consumer>
-    ) : (
-      <h2>
-        slug="
-        {propSlug}" doesn't return menu items.
-        <br />
-        Maybe you have a spelling error?
-      </h2>
-    );
-  } else {
-    return <h2>Add a WP menu slug to return menu items.</h2>;
-  }
-};
+//           // return children ? (
+//           //   children(items)
+//           // ) : (
+//           //   <>
+//           //     {items.map(item => (
+//           //       <Link key={`menu-item-${item.wordpress_id}`} to={item.url}>
+//           //         {item.title}
+//           //       </Link>
+//           //     ))}
+//           //   </>
+//           // );
+//         }}
+//       </Consumer>
+//     ) : (
+//       <h2>
+//         slug="
+//         {propSlug}" doesn't return menu items.
+//         <br />
+//         Maybe you have a spelling error?
+//       </h2>
+//     );
+//   } else {
+//     return <h2>Add a WP menu slug to return menu items.</h2>;
+//   }
+// };
