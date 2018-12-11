4b6a489e993f4b7b719a9e28f788ead098d3a947
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-10 21:00:06 -0800

    update api

--- src/components/flexible-content.js
+++ src/components/flexible-content.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
 class FlexibleContent extends React.Component {
   render() {
-    const { rows, components, meta } = this.props
+    const { rows, components, data } = this.props;
     if (!!rows || !!components) {
-      return rows.map(({ __typename: typename, ...data }, index) => {
-        const type = typename.replace('WordPressAcf_', '')
-        const Component = components[type]
+      return rows.map(({ __typename: typename, ...rowData }, index) => {
+        const type = typename.replace("WordPressAcf_", "");
+        const Component = components[type];
         return !!Component ? (
-          <Component key={index} {...data} {...meta} />
+          <Component key={index} {...rowData} {...data} />
         ) : (
           console.warn(`No component found for ${type} type`)
-        )
-      })
+        );
+      });
     } else {
-      return null
+      return null;
     }
   }
 }
 
-export default FlexibleContent
+export default FlexibleContent;
