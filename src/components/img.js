f5c4f393bd94dca24d624945c1cfcff0fbc118c5
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-12 13:41:47 -0800

    add image aspect ratio

--- src/components/img.js
+++ src/components/img.js
@@ -1,31 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import Img from "gatsby-image";
 
-const Wimg = ({ field, ...props }) => {
-  if (!field) {
-    return null;
+class Wimg extends Component {
+  constructor(props) {
+    super(props);
+
+    this.img = React.createRef();
+    this.imageLoaded = this.imageLoaded.bind(this);
+
+    this.state = {
+      imgAspect: false
+    };
   }
 
-  const { localFile: file } = field;
+  imageLoaded() {
+    const width = this.img.naturalWidth;
+    const height = this.img.naturalHeight;
 
-  const useStringUrl = typeof field === "string";
-  const useNestedStringUrl = !!field.url && typeof field.url === "string";
+    this.setState({ imgAspect: `${(height / width) * 100}%` });
+  }
+  render() {
+    const { field, ...props } = this.props;
 
-  if (useStringUrl || useNestedStringUrl) {
-    const stringUrl = useStringUrl ? field : field.url;
-    return <img src={stringUrl} {...props} />;
-  } else if (!!file && file.childImageSharp) {
-    if (file.childImageSharp.fluid) {
-      return <Img fluid={file.childImageSharp.fluid} {...props} />;
-    } else if (file.childImageSharp.fixed) {
-      return <Img fixed={file.childImageSharp.fixed} {...props} />;
+    if (!field) {
+      return null;
     }
-  } else {
-    console.warn(
-      `Wordsby Image requires a src url or a field containing a valid childImageSharp query`
-    );
-    return null;
+
+    const { localFile: file } = field;
+
+    const useStringUrl = typeof field === "string";
+    const useNestedStringUrl = !!field.url && typeof field.url === "string";
+
+    if (useStringUrl || useNestedStringUrl) {
+      const stringUrl = useStringUrl ? field : field.url;
+      return (
+        <div
+          className="gatsby-image-wrapper"
+          style={{ position: "relative", overflow: "hidden" }}
+        >
+          <div
+            style={{
+              width: "100%",
+              paddingBottom: this.state.imgAspect
+            }}
+          />
+          <img
+            ref={n => (this.img = n)}
+            onLoad={this.imageLoaded}
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "0px",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: "center center",
+              opacity: 1,
+              transition: "opacity 0.5s ease 0s"
+            }}
+            src={stringUrl}
+            {...props}
+          />
+        </div>
+      );
+    } else if (!!file && file.childImageSharp) {
+      if (file.childImageSharp.fluid) {
+        return <Img fluid={file.childImageSharp.fluid} {...props} />;
+      } else if (file.childImageSharp.fixed) {
+        return <Img fixed={file.childImageSharp.fixed} {...props} />;
+      }
+    } else {
+      console.warn(
+        `Wordsby Image requires a src url or a field containing a valid childImageSharp query`
+      );
+      return null;
+    }
   }
-};
+}
 
 export default Wimg;
