a2b01992b1c31a860e203d0a0b2aad1a7fae2295
Tyler <tyler@known.design>
2019-01-28 20:14:12 -0800

    add srcset support with the sizes prop to wordsby img

--- src/components/img.js
+++ src/components/img.js
@@ -20,7 +20,7 @@ class Wimg extends Component {
     this.setState({ imgAspect: `${(height / width) * 100}%` });
   }
   render() {
-    const { field, ...props } = this.props;
+    const { field, sizes, ...props } = this.props;
 
     if (!field) {
       return null;
@@ -38,15 +38,30 @@ class Wimg extends Component {
     const useNestedStringUrl = !!field.url && typeof field.url === "string";
 
     if (useStringUrl || useNestedStringUrl) {
-      const stringUrl = useStringUrl ? field : field.url;
-      const { style, className } = this.props;
+      let stringUrl = useStringUrl ? field : field.url;
+      let srcset = "";
+      if (sizes) {
+        // build srcset
+        for (let key of Object.keys(sizes)) {
+          if (key.includes("height")) {
+            continue;
+          } else if (key.includes("width")) {
+            srcset += `${sizes[key]}w, `;
+          } else {
+            srcset += `${sizes[key]} `;
+          }
+        }
+        // remove trailing comma
+        srcset = srcset.replace(/,\s*$/, "");
+
+        // set string url to medium url
+        stringUrl = sizes.medium;
+      }
 
       return (
         <div
-          className={`gatsby-image-wrapper wordsby-image-preview ${
-            className ? className : ""
-          }`}
-          style={{ position: "relative", overflow: "hidden", ...style }}
+          className="gatsby-image-wrapper"
+          style={{ position: "relative", overflow: "hidden" }}
         >
           <div
             style={{
@@ -65,16 +80,29 @@ class Wimg extends Component {
               height: "100%",
               objectFit: "cover",
               objectPosition: "center center",
+              fontFamily: `"object-fit: cover; object-position: 50% 50%"`,
               opacity: 1,
               transition: "opacity 0.5s ease 0s"
             }}
             src={stringUrl}
+            srcSet={srcset}
+            {...props}
           />
         </div>
       );
     } else if (!!file && file.childImageSharp) {
       if (file.childImageSharp.fluid) {
-        return <Img fluid={file.childImageSharp.fluid} {...props} />;
+        return (
+          <Img
+            fluid={file.childImageSharp.fluid}
+            imgStyle={{
+              objectFit: `cover`,
+              objectPosition: `50% 50%`,
+              fontFamily: `"object-fit: cover; object-position: 50% 50%"`
+            }}
+            {...props}
+          />
+        );
       } else if (file.childImageSharp.fixed) {
         return <Img fixed={file.childImageSharp.fixed} {...props} />;
       }
