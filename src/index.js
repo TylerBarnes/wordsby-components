5e68d1b97325389fd8c01b5b9ae577fad7fba1bf
Tyler <tyler@known.design>
2019-02-07 17:27:40 -0800

    move yoast to gatsby-plugin-wordsby since devs wont need to touch it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import FlexibleContent from "./components/flexible-content";
 import Img from "./components/img";
 import MenuItems from "./components/menu-items";
 import PsychicWindow from "./components/psychic-window";
-import Yoast from "./components/yoast";
 
-export { PsychicWindow, FlexibleContent, Img, MenuItems, Yoast };
+export { PsychicWindow, FlexibleContent, Img, MenuItems };
